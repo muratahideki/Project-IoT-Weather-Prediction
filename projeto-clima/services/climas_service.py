ca8528872482391962b8b9edf040457590e63e7a
Mario Hideki Murata <106937976+muratahideki@users.noreply.github.com>
2025-12-29 23:33:35 -0300

    Refactor rain probability calculation function
    Updated weights and bias in rain probability calculation.

--- projeto-clima/services/climas_service.py
+++ projeto-clima/services/climas_service.py
@@ -1,33 +1,17 @@
-def calcular_probabilidade_chuva(
-    temperatura,
-    umidade,
-    pressao,
-    vento
-):
-    """
-    Modelo simples heurístico
-    Retorna probabilidade em %
-    """
-    prob = 0
+def calcular_probabilidade_chuva(temp, umid, pressao, vento):
+    # --- NOVOS PESOS (Atualizado) ---
+    w_temp = -0.06074339
+    w_umid = 0.10755981
+    w_press = 0.00317959
+    w_vento = 0.26779365  # Novo peso do vento
+    vies = -10.9634014    # Novo viés
 
-    # Umidade alta
-    if umidade > 80:
-        prob += 30
-    elif umidade > 60:
-        prob += 15
+    # Equação Linear com 4 variáveis
+    z = (w_temp * temp) + (w_umid * umid) + (w_press * pressao) + (w_vento * vento) + vies
 
-    # Pressão baixa
-    if pressao < 1010:
-        prob += 25
-    elif pressao < 1015:
-        prob += 10
+    try:
+        probabilidade = 1 / (1 + math.exp(-z))
+    except OverflowError:
+        probabilidade = 0 if z < 0 else 1 
 
-    # Temperatura
-    if temperatura < 20:
-        prob += 10
-
-    # Vento
-    if vento > 8:
-        prob += 15
-
-    return min(prob, 100)
+    return probabilidade * 100 
