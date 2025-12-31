7b69218936e8a6c6d641c0f5f52c8a11139402b8
muratahideki@usp.br <Mario Hideki Murata>
2025-12-30 21:07:09 -0300

    arrumei a lógica

--- projeto-clima/app.py
+++ projeto-clima/app.py
@@ -19,7 +19,6 @@ scheduler.start()
 
 # Blueprints
 app.register_blueprint(api_bp)
-app.register_blueprint(dashboard_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- projeto-clima/routes/api.py
+++ projeto-clima/routes/api.py
@@ -1,5 +1,10 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, render_template, request, jsonify
 from repository.medidas_repo import salvar_medida, obter_medidas_brutas
+from services.vento_service import obter_vento_externo
+
+
+from repository.medidas_repo import obter_media_movel
+from services.climas_service import calcular_probabilidade_chuva
 
 api_bp = Blueprint("api", __name__)
 
@@ -7,13 +12,58 @@ api_bp = Blueprint("api", __name__)
 def receber_dados():
     dados = request.get_json()
 
+    # 1. Pega os dados do ESP32
+    temp = dados.get("temp")
+    press = dados.get("pressao")
+    alt = dados.get("altitude")
+    umid = dados.get("umidade")
+
+    vento_atual, _ = obter_vento_externo()
+
+
     if not dados:
         return jsonify({"erro": "JSON inválido"}), 400
 
-    salvar_medida(dados)
+    salvar_medida(temp, press, alt, umid, vento_atual)
     return jsonify({"status": "ok"})
 
 
 @api_bp.route("/api/brutos")
 def api_brutos():
     return jsonify(obter_medidas_brutas())
+
+
+@api_bp.route("/" )
+def dashboard():
+
+    media = obter_media_movel(60)
+
+    prob_chuva = 0.0
+    msg_previsao = "Aguardando dados..."
+    cor_card = "#eeeeee"
+    cor_texto = "#333333"
+    inputs_usados = [0, 0, 0, 0]
+
+    if media and media[0] is not None:
+        t, u, p, v = media
+        inputs_usados = [t, u, p, v]
+
+        prob_chuva = calcular_probabilidade_chuva(t, u, p, v)
+
+        if prob_chuva > 50:
+            cor_card = "#ffcccc"
+            cor_texto = "#a00000"
+            msg_previsao = "ALTA probabilidade de chuva"
+        else:
+            cor_card = "#ccffcc"
+            cor_texto = "#006600"
+            msg_previsao = "Baixa probabilidade de chuva"
+
+    return render_template(
+        "dashboard.html",
+        prob_chuva=prob_chuva,
+        msg_previsao=msg_previsao,
+        cor_card=cor_card,
+        cor_texto=cor_texto,
+        inputs_usados=inputs_usados,
+    )
